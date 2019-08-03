6d1a1fb8dbc57ea3e783c1230d072e664643e095
merdiano <merdan.m@gmail.com>
2019-08-04 01:53:06 +0500

    permissionssss

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,26 +1,12 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 
-@if(auth()->user()->hasPermissionTo('locations'))
+@hasrole('Admin')
 <li><a href='{{ backpack_url("location") }}'><i class='fa fa-globe'></i> <span>Locations</span></a></li>
-@endif
-@if(backpack_user()->hasPermissionTo('customers'))
+
 <li><a href='{{ backpack_url("client") }}'><i class='fa fa-users'></i> <span>Customers</span></a></li>
-@endif
-@if(backpack_user()->hasPermissionTo('categories'))
+
 <li><a href='{{ backpack_url("category") }}'><i class='fa fa-cubes'></i> <span>Categories</span></a></li>
-@endif
-@if(backpack_user()->hasPermissionTo('products'))
-<li><a href='{{ backpack_url("product") }}'><i class="fa fa-shopping-bag"></i> <span>Products</span></a></li>
-@endif
-@if(backpack_user()->hasPermissionTo('contacts'))
-<li><a href='{{ backpack_url("contact") }}'><i class="fa fa-envelope"></i> <span>Contacts</span></a></li>
-@endif
-@if(backpack_user()->hasPermissionTo('orders'))
-<li><a href='{{ backpack_url('order') }}'><i class='fa fa-tag'></i> <span>Orders</span></a></li>
-@endif
-<!-- Users, Roles Permissions -->
-@if(backpack_user()->hasPermissionTo('users'))
 <li class="treeview">
     <a href="#"><i class="fa fa-group"></i> <span>Users, Roles, Permissions</span> <i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
@@ -29,4 +15,13 @@
         <li><a href="{{ backpack_url('permission') }}"><i class="fa fa-key"></i> <span>Permissions</span></a></li>
     </ul>
 </li>
-@endif
+<li><a href='{{ backpack_url("contact") }}'><i class="fa fa-envelope"></i> <span>Contacts</span></a></li>
+@endhasrole
+@hasrole('Inventory Manager')
+<li><a href='{{ backpack_url("product") }}'><i class="fa fa-shopping-bag"></i> <span>Products</span></a></li>
+
+@endhasrole
+
+@hasrole('Sales Manager')
+<li><a href='{{ backpack_url('order') }}'><i class='fa fa-tag'></i> <span>Orders</span></a></li>
+@endhasrole
