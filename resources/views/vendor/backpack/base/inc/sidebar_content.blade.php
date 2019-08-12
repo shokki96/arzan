5b7a19b46fabe5cba455b8e906b832d6b1ebda61
merdiano <merdan.m@gmail.com>
2019-08-12 16:30:24 +0500

    permissions can() CHANGED TO backpack_user()->can()

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,16 +1,16 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 
-@can('locations')
+@if(backpack_user()->can('locations'))
 <li><a href='{{ backpack_url("location") }}'><i class='fa fa-globe'></i> <span>Locations</span></a></li>
-@endcan
-@can('customers')
+@endif
+@if(backpack_user()->can('customers'))
 <li><a href='{{ backpack_url("client") }}'><i class='fa fa-users'></i> <span>Customers</span></a></li>
-@endcan
-@can('categories')
+@endif
+@if(backpack_user()->can('categories'))
 <li><a href='{{ backpack_url("category") }}'><i class='fa fa-cubes'></i> <span>Categories</span></a></li>
-@endcan
-@can('users')
+@endif
+@if(backpack_user()->can('users'))
 <li class="treeview">
     <a href="#"><i class="fa fa-group"></i> <span>Users, Roles, Permissions</span> <i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
@@ -20,12 +20,12 @@
     </ul>
 </li>
 <li><a href='{{ backpack_url("contact") }}'><i class="fa fa-envelope"></i> <span>Contacts</span></a></li>
-@endcan
-@can('products')
+@endif
+@if(backpack_user()->can('products'))
 <li><a href='{{ backpack_url("product") }}'><i class="fa fa-shopping-bag"></i> <span>Products</span></a></li>
 
-@endcan
+@endif
 
-@can('orders')
+@if(backpack_user()->can('orders'))
 <li><a href='{{ backpack_url('order') }}'><i class='fa fa-tag'></i> <span>Orders</span></a></li>
-@endcan
+@endif
