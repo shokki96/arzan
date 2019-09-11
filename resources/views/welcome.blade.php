481e2f946b55b06d3306b50f9ae6c43003d327ed
Shohrat <shokki.a96@gmail.com>
2019-09-12 02:21:43 +0500

    front end welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,144 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<!-- Mirrored from getnajmul.com/theme/sassnex/v1/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Sep 2019 16:11:37 GMT -->
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="ARZAN, arzan, arzan tekstil, tekstil, arzan online, arzan tekstil, arzan tekstil online shop">
+    <meta name="author" content="AKAMI">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARZAN.COM.TM</title>
+    <!-- Goole Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/assets/bootstrap.min.css')}}">
+    <!-- Font awsome CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/assets/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/assets/magnific-popup.css')}}">    
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="{{asset('assets/css/assets/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/assets/owl.theme.css')}}">     
+    <link rel="stylesheet" href="{{asset('assets/css/assets/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/assets/preloader.css')}}"> 
+    <!-- Slick Carousel -->
+    <link rel="stylesheet" href="{{asset('assets/css/assets/slick.css')}}">  
+    <!-- Mean Menu-->
+    <link rel="stylesheet" href="{{asset('assets/css/assets/meanmenu.css')}}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- main style-->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">    
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+                     
+</head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+<body>
+<div id="loader-wrapper">
+    <div id="loader"></div>
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
+</div>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<div id="intro_detault" class="header_app">
+    <div class="intro_wrapper">
+        <div class="container">  
+            <div class="row">        
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="intro_banner">
+                       <div class="screenshot">
+                           <img src="{{asset('assets/images/screenshot/banner_1.png')}}" alt="" class="screen_1  wow fadeInUp animated" data-wow-duration="2s" data-wow-delay=".2s">
+                           <img src="{{asset('assets/images/screenshot/banner_2.png')}}" alt="" class="screen_2  wow fadeInUp animated" data-wow-duration="2s" data-wow-delay=".5s">
+                           <img src="{{asset('assets/images/screenshot/banner_3.png')}}" alt="" class="screen_3  wow fadeInUp animated" data-wow-duration="2s" data-wow-delay=".8s">
+                       </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                 <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="intro_text">
+                        <h4>Amatly we Ýokary Hilli Harytlar</h4>
+                        <h1 class="wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">ARZAN  PROGRAMMASYNDA</h1>
+                        <p class="wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">ARZAN programmasy Android we Ios telefonlary üçin niýetlendir.Bu programma arkaly programmadaky harytlarymyzy online görnüşde sargyt edip bilersiňiz.</p>
+                        <div class="download_app_icon d-flex  wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
+                            <a href="https://apps.apple.com/us/app/arzan/id1477392853?l=ru&ls=1" target="_blank" class=""><img src="{{asset('assets/images/app_store.png')}}" alt=""></a>                 
+                            <a href="https://play.google.com/store/apps/details?id=com.akami.arzan" target="_blank" class=""><img src="{{asset('assets/images/play_store.png')}}" alt=""></a>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div> 
+    </div>  
+    <div class="banner_shapes">
+        <img src="{{asset('assets/images/shapes/app_1.png')}}" alt="" class="app_1">
+        <img src="{{asset('assets/images/shapes/app_2.png')}}" alt="" class="app_2">        
+        <img src="{{asset('assets/images/shapes/app_3.png')}}" alt="" class="app_3">
+        <img src="{{asset('assets/images/shapes/app_4.png')}}" alt="" class="app_4">        
+        <img src="{{asset('assets/images/shapes/pen.png')}}" alt="" class="pen  wow fadeInLeftBig animated" data-wow-duration="2s" data-wow-delay=".2s">
+    </div>
+</div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+
+
+<!-- App Download Area -->  
+<div class="app_download">
+    <div class="container">
+        <div class="row single_features">
+            <div class="col-12 col-md-6 col-lg-6">
+                <div class="feature_banner wow fadeInLeft" data-wow-duration="2s" data-wow-delay=".3s">
+                    <img src="{{asset('assets/images/illustration/app_d.png')}}" alt="" class="right_banner">
+                    <div class="banner_shapes">
+                        <img src="{{asset('assets/images/shapes/app_4.png')}}" alt="" class="app_1">
+                        <img src="{{asset('assets/images/shapes/app_3.png')}}" alt="" class="app_3">
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6">
+                <div class="feature_intro_wrapper">    
+                    <div class="feature_intro">
+                        <h2 class=" wow fadeInRight" data-wow-duration="2s" data-wow-delay=".2s">ARZAN</h2>
+                        <p class=" wow fadeInRight" data-wow-duration="2s" data-wow-delay=".3s">Amatly we ýokary hilli harytlar we Gysga wagtyň içinde eltip berme hyzmaty ARZAN programmasynda.</p>
+                    </div>
+                    <div class="download_app_icon d-flex  wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
+                        <a href="https://apps.apple.com/us/app/arzan/id1477392853?l=ru&ls=1" target="_blank" class=""><img src="{{asset('assets/images/app_store2.png')}}" alt=""></a>                 
+                        <a href="https://play.google.com/store/apps/details?id=com.akami.arzan" target="_blank" class=""><img src="{{asset('assets/images/play_store2.png')}}" alt=""></a>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</div>
+
+<!-- Footer -->  
+<footer id="footer_app">
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Copyright  &copy; ARZAN</p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="copy_right_items d-flex justify-content-end">
+                        <li><a href="#">+993 65 809786</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+   
+</footer><!-- End Footer -->
+
+
+    <!-- JavaScript -->
+    <script src="{{asset('assets/js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>     
+    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>  
+    <script src="{{asset('assets/js/slick.min.js')}}"></script>  
+    <!-- Main Menu -->
+    <script src="{{asset('assets/js/jquery.meanmenu.min.js')}}"></script>   
+    <script src="{{asset('assets/js/custom.js')}}"></script>        
+</body>
+
+<!-- Mirrored from getnajmul.com/theme/sassnex/v1/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Sep 2019 16:13:33 GMT -->
 </html>
