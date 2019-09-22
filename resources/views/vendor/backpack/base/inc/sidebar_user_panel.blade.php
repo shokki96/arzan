c4564ea09ddf76cc3c280e629fe432cf23a2a84a
tmStore <tmstore@iMac-tmStore.local>
2019-09-22 17:33:59 +0500

    profile setting 3

--- resources/views/vendor/backpack/base/inc/sidebar_user_panel.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_user_panel.blade.php
@@ -1,9 +1,9 @@
-<div class="user-panel">
+<div class="user-panel" style="background-color: #1b2427;text-align: center">
   {{-- <a class="pull-left image" href="{{ route('backpack.account.info') }}">
     <img src="{{ backpack_avatar_url(backpack_auth()->user()) }}" class="img-circle" alt="User Image">
   </a> --}}
-  <div class="pull-left">
-    <p style="margin-bottom: 0"><a href="{{ route('backpack.account.info') }}" style="color: #fff; font-weight: 500;">{{ backpack_auth()->user()->name }}</a></p>
+  <div class="pull-left" style="width: 100%">
+    <p style="margin-bottom: 0"><a href="{{ route('backpack.account.info') }}" style="color: #fff; font-weight: 500;font-size: 17px;">{{ backpack_auth()->user()->name }}</a></p>
     <small><small><a href="{{ route('backpack.account.info') }}"><span><i class="fa fa-user-circle-o"></i> {{ trans('backpack::base.my_account') }}</span></a> &nbsp;  &nbsp; <a href="{{ backpack_url('logout') }}"><i class="fa fa-sign-out"></i> <span>{{ trans('backpack::base.logout') }}</span></a></small></small>
   </div>
 </div>
