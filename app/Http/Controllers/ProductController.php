1cc3c6b15249d3dbc04aaccaf31f32b55f075cbf
merdiano <merdan.m@gmail.com>
2019-10-20 18:09:23 +0500

    product paginate 12

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
         }
         //        todo sorting???
         return $query->orderBy('created_at','DESC')
-            ->paginate(6);
+            ->paginate(12);
     }
 
 
